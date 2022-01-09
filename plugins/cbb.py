928661645248c3aada554bdec67a59c7fbc03c82
Iamsreerag <89203874+Iamsreerag@users.noreply.github.com>
2022-01-09 21:33:10 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Channel : <a href='https://t.me/joinchat/slPWoPDfoJc3NTVl'>♏𝗮𝗹𝗮𝘆𝗮𝗹𝗮𝗺_𝗖𝗶𝗻𝗲𝗺𝗮</a>\n○ malayalam_movies : <code>Python3</code>\n○ Library : <a href='https://t.me/joinchat/slPWoPDfoJc3NTVl'>Pyrogram asyncio {__version__}</a>\n○ Series World : <a href='https://t.me/mc_serie'>Click here</a>\n○ Channel : @MALAYAYALIES\n○ Support Group : @malayalam2</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
